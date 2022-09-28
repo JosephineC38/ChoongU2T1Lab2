9d2049d6def4ccb32757dd523ae53b7b9d216873
JosephineC38 <josephinec38@nycstudents.net>
2022-09-28 11:24:33 -0400

    Added answers

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,13 +1,17 @@
 public class CircleRunner {
     public static void main(String[] args) {
         // A. create a Circle object with radius 5.0
+        Circle New = new Circle(5);
 
         // B. locate the method defined in the Circle class and write
         //    a line of code to call it on your object
+        New.printArea();
 
         // C. BEFORE running this code, PREDICT what gets printed
+        // It will print 78.5
 
         // D. run the code and confirm your prediction! Were you correct?
+        // My calculation was correct but I forgot about the "My area is: "
     }
 }
 
